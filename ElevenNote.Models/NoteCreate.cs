8db3d38cdd0f9ba4c52045e1f56622d81aa6a6ba
Peter-Nikolov1 <nikolov_peter@yahoo.com>
2022-01-18 20:02:05 -0500

    Updated Layout View

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter more than 2 characters.")]
+        [MaxLength(40, ErrorMessage ="Please delete some of your title!")]
+        public string Title { get; set; }
+
+        [MaxLength(5000)]
+        public string Content { get; set; }
+    }
+}
